8bcebf2914d114b5563dd87b47cfdcb5c5b19bc1
Jordan Daly <jordandaly@gmail.com>
2018-06-18 21:54:20 +1200

    added manage static data route, functions and template to view and add new static data

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 # create an instance of flask = app variable
 app = Flask(__name__)
@@ -15,14 +15,91 @@ heroku = Heroku(app)
 
 #app.config.from_pyfile('models.py')
 
-from models import db, Recipe
+from models import db, Recipe, Category, Course, Cuisine, Country, Allergen, Dietary
 
 #############################HOME PAGE##########################################
 @app.route('/')
 def index():
     recipe_count = Recipe.query.count()
-    recipes_list = Recipe.query.limit(30).all()
+    recipes_list = Recipe.query.limit(100).all()
     return render_template('index.html', recipe_count=str(recipe_count), recipes_list=recipes_list)
+
+# @app.route('/add_recipe', methods = ['GET','POST'])
+# def add_recipe():
+#     recipe = Recipe(request.form['recipe_name'])
+#     db.session.add(category)
+#     db.session.commit()
+#     return redirect(url_for('index'))
+
+#############################MANAGE STATIC DATA##########################################
+@app.route('/manage_static_data')
+def manage_static_data():
+    category_count = Category.query.count()
+    categories_list = Category.query.limit(100).all()
+
+    course_count = Course.query.count()
+    courses_list = Course.query.limit(100).all()
+
+    cuisine_count = Cuisine.query.count()
+    cuisines_list = Cuisine.query.limit(100).all()
+
+    country_count = Country.query.count()
+    countries_list = Country.query.limit(250).all()
+
+    allergen_count = Allergen.query.count()
+    allergens_list = Allergen.query.limit(100).all()
+
+    dietary_count = Dietary.query.count()
+    dietaries_list = Dietary.query.limit(100).all()
+    return render_template('manage_static_data.html', category_count=str(category_count), categories_list=categories_list, course_count=str(course_count), courses_list=courses_list, cuisine_count=str(cuisine_count), cuisines_list=cuisines_list, country_count=str(country_count), countries_list=countries_list, allergen_count=str(allergen_count), allergens_list=allergens_list, dietary_count=str(dietary_count), dietaries_list=dietaries_list)
+
+#############################CATEGORY##########################################
+@app.route('/add_category', methods = ['POST'])
+def add_category():
+    category = Category(request.form['category'])
+    db.session.add(category)
+    db.session.commit()
+    return redirect(url_for('manage_static_data'))
+
+#############################COURSE##########################################
+@app.route('/add_course', methods = ['POST'])
+def add_course():
+    course = Course(request.form['course'])
+    db.session.add(course)
+    db.session.commit()
+    return redirect(url_for('manage_static_data'))
+
+#############################CUISINE##########################################
+@app.route('/add_cuisine', methods = ['POST'])
+def add_cuisine():
+    cuisine = Cuisine(request.form['cuisine'])
+    db.session.add(cuisine)
+    db.session.commit()
+    return redirect(url_for('manage_static_data'))
+
+#############################COUNTRY##########################################
+@app.route('/add_country', methods = ['POST'])
+def add_country():
+    country = Country(request.form['country'])
+    db.session.add(country)
+    db.session.commit()
+    return redirect(url_for('manage_static_data'))
+
+#############################ALLERGEN##########################################
+@app.route('/add_allergen', methods = ['POST'])
+def add_allergen():
+    allergen = Allergen(request.form['allergen'])
+    db.session.add(allergen)
+    db.session.commit()
+    return redirect(url_for('manage_static_data'))
+
+#############################DIETARY##########################################
+@app.route('/add_dietary', methods = ['POST'])
+def add_dietary():
+    dietary = Dietary(request.form['dietary'])
+    db.session.add(dietary)
+    db.session.commit()
+    return redirect(url_for('manage_static_data'))
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -19,36 +19,86 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(String(150), nullable=False, unique=True)
 
-    recipes = db.relationship('Recipe', backref='category', lazy='dynamic')
+    #recipes = db.relationship('Recipe', backref='category', lazy='dynamic')
+
+    def __init__(self, category_name):
+        self.category_name = category_name
+
+    def __repr__(self):
+        return '<Category>' % self.category_name
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     course_name = db.Column(String(150), nullable=False, unique=True)
 
-    recipes = db.relationship('Recipe', backref='course', lazy='dynamic')
+    #recipes = db.relationship('Recipe', backref='course', lazy='dynamic')
+
+    def __init__(self, course_name):
+        self.course_name = course_name
+
+    def __repr__(self):
+        return '<Course>' % self.course_name
 
 class Cuisine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cuisine_name = db.Column(String(150), nullable=False, unique=True)
 
-    recipes = db.relationship('Recipe', backref='cuisine', lazy='dynamic')
+    #recipes = db.relationship('Recipe', backref='cuisine', lazy='dynamic')
+
+    def __init__(self, cuisine_name):
+        self.cuisine_name = cuisine_name
+
+    def __repr__(self):
+        return '<Cuisine>' % self.cuisine_name
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author_name = db.Column(String(150), nullable=False, unique=True)
-    author_country = db.Column(String(150))
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
+    country = db.relationship('Country', backref=db.backref('authors', lazy=True))
 
-    recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
+    #recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
+
+    def __init__(self, author_name):
+        self.author_name = author_name
+    
+    def __repr__(self):
+        return '<Author>' % self.author_name
+
+class Country(db.Model):
+    id = Column(db.Integer, primary_key=True)
+    country_name = db.Column(String(150), nullable=False, unique=True)
+    
+    def __init__(self, country_name):
+        self.country_name = country_name
+    
+    def __repr__(self):
+        return '<Country>' % self.country_name
 
 class Step(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    recipe = db.relationship('Recipe', backref=db.backref('steps', lazy=True))
     step_number = db.Column(db.Integer)
     step_description = db.Column(Text)
+
+    def __init__(self, step_number, step_description, recipe):
+        self.step_number = step_number
+        self.step_description = step_description
+        self.recipe = recipe
+    
+    def __repr__(self):
+        return '<Step>' % self.step_description
 
 class Allergen(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     allergen_name = db.Column(String(150), nullable=False, unique=True)
+
+    def __init__(self, allergen_name):
+        self.allergen_name = allergen_name
+    
+    def __repr__(self):
+        return '<Allergen>' % self.allergen_name
 
 db.Table('recipe_allergen',
     db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
@@ -58,6 +108,12 @@ db.Table('recipe_allergen',
 class Dietary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     dietary_name = db.Column(String(150), nullable=False, unique=True)
+
+    def __init__(self, dietary_name):
+        self.dietary_name = dietary_name
+    
+    def __repr__(self):
+        return '<Dietary>' % self.dietary_name
 
 db.Table('recipe_dietary',
     db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
@@ -71,15 +127,35 @@ class Recipe(db.Model):
     preparation_time = db.Column(db.Integer)
     cooking_time = db.Column(db.Integer)
     servings = db.Column(db.Integer)
+    
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref=db.backref('recipes', lazy=True))
+    
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    course = db.relationship('Course', backref=db.backref('recipes', lazy=True))
+    
     cuisine_id = db.Column(db.Integer, db.ForeignKey('cuisine.id'), nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    cuisine = db.relationship('Cuisine', backref=db.backref('recipes', lazy=True))
 
-    steps = db.relationship('Step', backref='recipe', lazy='dynamic')
-    quantities = db.relationship('Quantity', backref='recipe', lazy='dynamic')
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    author = db.relationship('Author', backref=db.backref('recipes', lazy=True))
+
+    #steps = db.relationship('Step', backref='recipe', lazy='dynamic')
+    #quantities = db.relationship('Quantity', backref='recipe', lazy='dynamic')
+    
     allergens = db.relationship('Allergen', secondary='recipe_allergen', backref='recipe', lazy='dynamic')
     dietaries = db.relationship('Dietary', secondary='recipe_dietary', backref='recipe', lazy='dynamic')
+
+    def __init__(self, recipe_name, recipe_description, preparation_time, cooking_time, servings, category, course, cuisine, author):
+        self.recipe_name = recipe_name
+        self.recipe_description = recipe_description
+        self.preparation_time = preparation_time
+        self.cooking_time = cooking_time
+        self.servings = servings
+        self.category = category
+        self.course = course
+        self.cuisine = cuisine
+        self.author = author
 
     def __repr__(self):
         return '<Recipe>' % self.recipe_name
@@ -88,19 +164,47 @@ class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ingredient_name = db.Column(String(150), nullable=False, unique=True)
 
-    quantities = db.relationship('Quantity', backref='ingredient', lazy='dynamic')
+    #quantities = db.relationship('Quantity', backref='ingredient', lazy='dynamic')
+
+    def __init__(self, ingredient_name):
+        self.ingredient_name = ingredient_name
+    
+    def __repr__(self):
+        return '<Ingredient>' % self.ingredient_name
 
 class Measurement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     measurement_name = db.Column(String(150), nullable=False, unique=True)
 
-    quantities = db.relationship('Quantity', backref='measurement', lazy='dynamic')
+    #quantities = db.relationship('Quantity', backref='measurement', lazy='dynamic')
+
+    def __init__(self, ingredient_name):
+        self.measurement_name = measurement_name
+    
+    def __repr__(self):
+        return '<Measurement>' % self.measurement_name
 
 
 class Quantity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
-    measurement_id = db.Column(db.Integer, db.ForeignKey('measurement.id'), nullable=False)
     quantity = db.Column(db.Float)
+    
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    recipe = db.relationship('Recipe', backref=db.backref('quantities', lazy=True))
+    
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
+    ingredient = db.relationship('Ingredient', backref=db.backref('quantities', lazy=True))
+    
+    measurement_id = db.Column(db.Integer, db.ForeignKey('measurement.id'), nullable=False)
+    measurement = db.relationship('Measurement', backref=db.backref('quantities', lazy=True))
+
+    def __init__(self, quantity, recipe, ingredient, measurement):
+        self.quantity = quantity
+        self.recipe = recipe
+        self.ingredient = ingredient
+        self.measurement = measurement
+    
+    def __repr__(self):
+        return '<Quantity>' % self.quantity
+    
     
